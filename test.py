629231ef5bd903bae0d973b8bf0a0a938fab5cf2
unknown <lukas.connert@gmail.com>
2021-02-15 18:27:05 +0100

    Length of active bandits

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-ideal_flexions = [1, 3, 4, 2, 1, 5]
-max_ideal = np.array(ideal_flexions[4:6])
-print(max_ideal[0])
+import pickle
+
+bandits = pickle.load(open('activebandits_0217_5.pkl','rb'))
+print(len(bandits))
 
